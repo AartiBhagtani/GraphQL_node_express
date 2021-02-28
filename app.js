785fadb4f1a3300d104abe330442ce2003fe8834
Aarti Bhagtani <bhagtaniaarti@gmail.com>
2021-02-28 13:25:04 +0530

    add create post, add authentication, login schemas and resolvers

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const {graphqlHTTP} = require('express-graphql');
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
 
+const auth = require('./middleware/auth')
+
 require('dotenv').config()
 
 const app = express();
@@ -38,6 +40,9 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if(req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
   next();
 });
 
@@ -49,10 +54,23 @@ app.use((error, req, res, next) => {
   res.status(status).json({message: message, data: data});
 })
 
+app.use(auth);
+
 app.use('/graphql', graphqlHTTP({
   schema: graphqlSchema,
   rootValue: graphqlResolver,
-  graphiql: true
+  graphiql: true,
+  customFormatErrorFn(err) {
+    // default err is return
+    // return err;
+    if(!err.originalError) {
+      return err;
+    }
+    const data = err.originalError.data;
+    const message = err.message || 'An error occurred';
+    const code = err.originalError.code || 500;
+    return {message: message, status: code, data:data}
+  }
 }));
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {useUnifiedTopology: true, useNewUrlParser: true})
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,28 @@
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
 const validator = require('validator');
 
 const User = require('../models/user')
+const Post = require('../models/post')
 
 // as we have only single route /graphql input data validation is done here
 
 module.exports = {
   createUser: async function({userInput}, req) {
+    const errors = []
+    if(!validator.isEmail(userInput.email)){
+      errors.push({message: 'Email Id is invalid'})
+    }
+    if(validator.isEmpty(userInput.password) || 
+      !validator.isLength(userInput.password, {min: 5})) {
+        errors.push({message: 'Password too short'});
+    }
+    if(errors.length > 0) {
+      const error = new Error('Invalid input');
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
     // const email = args.userInput.email;
     const existingUser = await User.findOne({email: userInput.email})
     if(existingUser) {
@@ -22,5 +37,68 @@ module.exports = {
     })
     const createdUser = await user.save();
     return { ...createdUser._doc, _id: createdUser._id.toString() }
+  },
+
+  login: async function({email, password}) {
+    const user = await User.findOne({email: email});
+    if(!user) {
+      const error = new error('Invalid email');
+      error.code = 401;
+      throw error;
+    }
+    const isEqual = await bcrypt.compare(password, user.password)
+    if(!isEqual) {
+      const error = new error('Invalid Password');
+      error.code = 401;
+      throw error;
+    }
+    const token = jwt.sign({
+      userId: user._id.toString(),
+      email: user.email
+    }, 'secretsecret', {expiresIn: '1h'})
+
+    return {token: token, userId: user._id.toString()}
+  }, 
+
+  createPost: async function({postInput}, req) {
+    if(!req.isAuth) {
+      const error = new Error('Not Authenticated!');
+      error.code = 401;
+      throw error;
+    }
+    const errors = []
+    if(validator.isEmpty(postInput.title) || !validator.isLength(postInput.title, {min: 3} )){
+      errors.push({message: 'Title should atleast 3 letters'})
+    }
+    if(validator.isEmpty(postInput.content) || !validator.isLength(postInput.content, {min: 4} )){
+      errors.push({message: 'Content should atleast 4 letters'})
+    }
+    if(errors.length > 0) {
+      const error = new Error('Invalid input');
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+    const user = await User.findById(req.userId)
+    if(!user) {
+      const error = new Error('Invalid user');
+      error.code = 401;
+      throw error;
+    }
+    const post = new Post({
+      title: postInput.title,
+      imageUrl: postInput.imageUrl,
+      content: postInput.content, 
+      creator: user
+    })
+    const createdPost = await post.save();
+    user.posts.push(createdPost);
+    await user.save()
+    return { 
+      ...createdPost._doc, 
+      _id: createdPost._id.toString(),
+      createdAt: createdPost.createdAt.toISOString(),
+      updatedAt: createdPost.updatedAt.toISOString()
+    };
   }
- }
+ };
